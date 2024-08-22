98f7c33308bc25bf0550e19d7146ce2f1cb62f26
JustPowerful <ahmedaminedoudech@gmail.com>
2024-08-22 14:11:28 +0100

    chore: add the newest version of next and add shadcn/ui

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
-// import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="text-xl">Initial page</div>;
+  return (
+    <main className="p-6">
+      Here's a shadcn/ui test for ya! <br />
+      <Button>Click Me</Button> <br />
+      <br />
+      <br />
+      <div>
+        Note: this is a full boilerplate that contains different libraries and
+        tools for both the frontend and backend. - for the frontend we're using
+        NextJS with TypeScript and TailwindCSS - for the backend we're using
+        Fastify with TypeScript with a great setup for autoloading routes,
+        plugins and schemas!
+        <br />
+        <div className="text-3xl">Have fun!</div>
+      </div>
+    </main>
+  );
 }
